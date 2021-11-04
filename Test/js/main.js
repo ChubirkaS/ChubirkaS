34b2ed27fe31ae54ac87c732a8a452c560ee198b
Serhii Chubirka <Chubirka11@icloud.com>
2021-11-04 12:29:02 +0200

    Test-S

---
+++ Test/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+$('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
+});
+
+});
